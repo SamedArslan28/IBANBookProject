f107098a94eaaf884b57958a3b9b2e8a09f61efb
Gökhan Subaşı <64008090+gokhansubasi@users.noreply.github.com>
2024-01-09 19:01:59 +0300

    hata mesajı

--- IbanBookProject/Common/Extenstions/ColorExtension.swift
+++ IbanBookProject/Common/Extenstions/ColorExtension.swift
@@ -14,8 +14,16 @@ extension UIColor {
     static var themeColor: UIColor {
         return UIColor(red: 0.9450980392156862, green: 0.35294117647058826, blue: 0.1843137254901961, alpha: 1)
     }
-
+    
     static var appBackgroundColor: UIColor{
         return UIColor(red: 0.9254901960784314, green: 0.9254901960784314, blue: 0.9254901960784314, alpha: 1)
+    }
+    
+    static var colorOne: UIColor{
+        return UIColor(red: 238/255, green: 224/255, blue: 229/255, alpha: 1)
+    }
+    
+    static var colorTwo: UIColor{
+        return UIColor(red: 250/255, green: 240/255, blue: 230/255, alpha: 1)
     }
 }
